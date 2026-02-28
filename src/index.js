17b0a21301ef1313449cb6630afa20340d0a2708
MorelliCode <110130671+MorelliCode@users.noreply.github.com>
2026-02-28 20:55:41 -0300

    🎨 Place db initialization into database file

--- src/index.js
+++ src/index.js
@@ -13,29 +13,11 @@ const PORT = process.env.PORT || 3001;
 
 console.log('Wallet service starting...');
 
-
-
-// Initialize database
-const initDb = async () => {
-  await db.query(`
-    CREATE TABLE IF NOT EXISTS transactions (
-      id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-      user_id VARCHAR(255) NOT NULL,
-      amount INTEGER NOT NULL,
-      type VARCHAR(10) CHECK (type IN ('CREDIT', 'DEBIT')) NOT NULL,
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
-  `);
-};
-
-
-
-
 // Start the server
 const startServer = async () => {
     try {
         console.log('Connecting to database and initializing...');
-        await initDb();
+        await db.initDb();
         console.log('Database initialized successfully.');
 
         app.listen(PORT, () => {
