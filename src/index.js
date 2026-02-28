be2ae0d33369c94f0ebce89d828c4be6fab12747
MorelliCode <110130671+MorelliCode@users.noreply.github.com>
2026-02-28 19:19:27 -0300

    ✨ Implement get method for balance endpoint

--- src/index.js
+++ src/index.js
@@ -76,7 +76,7 @@ app.get('/transactions', authenticateToken, async (request, response) => {
     try {
         let result;
         if (type) {
-            result = await db.query('SELECT * FROM transactions WHERE type = $1', [type]);
+            result = await db.query('SELECT * FROM transactions WHERE type = $1', [type.toUpperCase()]);
         } else {
             result = await db.query('SELECT * FROM transactions');
         }
@@ -86,6 +86,28 @@ app.get('/transactions', authenticateToken, async (request, response) => {
     };
 });
 
+
+// GET /balance
+app.get('/balance', authenticateToken, async (request, response) => {
+    try {
+        const query = `
+            SELECT
+                SUM(CASE WHEN type = 'CREDIT' THEN amount ELSE 0 END) -
+                SUM(CASE WHEN type = 'DEBIT' THEN amount ELSE 0 END) AS amount
+            FROM transactions
+        `;
+        const result = await db.query(query);
+
+        let { amount } = result.rows[0];
+        if (amount === null) {
+            amount = 0;
+        }
+
+        response.status(200).json({ amount });
+    } catch (error) {
+        response.status(500).json({ error: 'Calculation Error' });
+    };
+});
 
 // Start the server
 const startServer = async () => {
